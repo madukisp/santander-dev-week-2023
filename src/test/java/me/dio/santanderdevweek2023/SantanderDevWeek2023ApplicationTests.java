2a482e88b63f2a734ef8619929a88dc55afea5ce
Amanda Duque <madukisp@gmail.com>
2023-10-14 17:13:48 -0300

    Initial commit

---
+++ src/test/java/me/dio/santanderdevweek2023/SantanderDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santanderdevweek2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
