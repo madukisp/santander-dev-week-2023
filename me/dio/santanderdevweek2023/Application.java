ccec40eda81807b15518e489671aad9eb4fd7185
Amanda DuKawa <110678035+madukisp@users.noreply.github.com>
2023-10-14 18:08:06 -0300

    Create README.md

--- me/dio/santanderdevweek2023/Application.java
+++ me/dio/santanderdevweek2023/Application.java
@@ -3,7 +3,6 @@ package me.dio.santanderdevweek2023;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
